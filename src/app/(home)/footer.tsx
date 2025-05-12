032a50515454ffee36e2f0a2005d0c6d8434f752
TilokDutta <tilokdutta1234@gmail.com>
2025-05-12 20:34:34 +0530

    03: Home Layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+        <footer className='flex border-t justify-between font-medium p-6'>
+            <div className='flex items-center gap-2'>
+                <p>funcroad, Inc.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default footer
