f446bc0b44d89d417c0cd477bc9b863c1d0cd2a7
TilokDutta <tilokdutta1234@gmail.com>
2025-05-16 19:56:06 +0530

    06: categories finalization

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -7,9 +7,11 @@ import { Category } from "@/payload-types"
 import { useRef, useState } from "react"
 import { useDropdownPosition } from "./use-dropdown-position"
 import { SubcategoryMenu } from "./subcategory-menu"
+import { CustomCategory } from "./types"
+import Link from "next/link"
 
 interface Props{
-    category:Category,
+    category:CustomCategory,
     isActive?:boolean,
     isNavigationHovered?:boolean
 }
@@ -29,6 +31,12 @@ export const CategoryDropdown = ({
         }
     }
     const onMouseLeave = () => setIsOpen(false);
+    //Potentially Improve Mobile experience by adding a delay to the dropdown opening
+    const toggleDropdown = () => {
+        if(category.subcategories.length){
+            setIsOpen(!isOpen);
+        }
+    }
 
     const dropdownPosition = getDropdownPosition();
     return(
@@ -37,16 +45,23 @@ export const CategoryDropdown = ({
             ref={dropdownRef}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
+            onClick={toggleDropdown}
         >
             <div className="relative">
                 <Button 
                     variant="elevated"
                     className={cn(
                         "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black", 
-                        isActive && !isNavigationHovered && "bg-white border-primary"
-                    )}
+                        isActive && !isNavigationHovered && "bg-white border-primary",
+                        isOpen && "bg-white shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px] border-primary"
+                    )} 
                 >
-                    {category.name}
+                    <Link
+                        // prefetch
+                        href={`/${category.slug === 'all' ? "": category.slug}`}
+                    >
+                        {category.name}
+                    </Link>
                 </Button>
                 {category.subcategories && category.subcategories.length > 0 && (
                     <div
--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,20 +1,38 @@
+"use client"
+
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { CustomCategory } from "./types";
+import { CategorySidebar } from "./category-sidebar";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 interface Props{
     disabled?:boolean;
+    data:CustomCategory[];
 }
 
 export const SearchInput = ({
-    disabled
+    disabled,
+    data
 }:Props) => {
+
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false)
+
   return (
     <div className="flex items-center gap-2 w-full">
+      <CategorySidebar data={data} open={isSideBarOpen} onOpenChange={setIsSideBarOpen}/>
         <div className="relative w-full">
             <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-600"/>
-            <Input className="pl-8" placeholder="Search Products"/>
+            <Input className="pl-8" placeholder="Search Products" disabled={disabled} />
         </div>
-
+        <Button
+          variant="elevated"
+          className="size-12 shrink-0 flex lg:hidden"
+          onClick={() => setIsSideBarOpen(true)}
+        >
+          <ListFilterIcon/>
+        </Button>
     </div>
-  )
-}
+  );
+};
