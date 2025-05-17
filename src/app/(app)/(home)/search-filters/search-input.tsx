2d0aa8864a29f3638f3979c80672fc74be5f6531
TilokDutta <tilokdutta1234@gmail.com>
2025-05-18 02:20:39 +0530

    07: trpc intergration

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -2,26 +2,23 @@
 
 import { Input } from "@/components/ui/input";
 import { ListFilterIcon, SearchIcon } from "lucide-react";
-import { CustomCategory } from "./types";
 import { CategorySidebar } from "./category-sidebar";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 interface Props{
     disabled?:boolean;
-    data:CustomCategory[];
 }
 
 export const SearchInput = ({
     disabled,
-    data
 }:Props) => {
 
   const [isSideBarOpen, setIsSideBarOpen] = useState(false)
 
   return (
     <div className="flex items-center gap-2 w-full">
-      <CategorySidebar data={data} open={isSideBarOpen} onOpenChange={setIsSideBarOpen}/>
+      <CategorySidebar open={isSideBarOpen} onOpenChange={setIsSideBarOpen}/>
         <div className="relative w-full">
             <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-600"/>
             <Input className="pl-8" placeholder="Search Products" disabled={disabled} />
