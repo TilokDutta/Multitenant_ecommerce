8b38f273790daf562c58ca91a38138bab81a2fe5
TilokDutta <tilokdutta1234@gmail.com>
2025-05-24 01:19:22 +0530

    12: products

--- src/modules/categories/server/procedure.ts
+++ src/modules/categories/server/procedure.ts
@@ -21,7 +21,6 @@ export const categoriesRouter = createTRPCRouter({
             subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
               //Because of "depth": 1 we are confident that "doc" will be a category
               ...(doc as Category),
-              subcategories:undefined,
             }))
           }));
 
